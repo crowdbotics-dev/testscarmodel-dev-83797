e2a1789494848d76ddef0c913708b0f0fe9a676e
Crowdbotics <engineering@crowdbotics.com>
2023-05-16 12:42:20 +0000

    Update store.

--- store/testscarmodeldevAPI/api.js
+++ store/testscarmodeldevAPI/api.js
@@ -3,6 +3,12 @@ const testscarmodeldevAPI = axios.create({
   baseURL: "https://testscarmodel-dev-83797-prod.herokuapp.com",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
+function api_v1_scar_list(payload) {
+  return testscarmodeldevAPI.get(`/api/v1/scar/`)
+}
+function api_v1_scar_create(payload) {
+  return testscarmodeldevAPI.post(`/api/v1/scar/`, payload.data)
+}
 function api_v1_login_create(payload) {
   return testscarmodeldevAPI.post(`/api/v1/login/`, payload.data)
 }
@@ -25,6 +31,18 @@ function api_docs_schema_retrieve(payload) {
 }
 function rest_auth_login_create(payload) {
   return testscarmodeldevAPI.post(`/rest-auth/login/`, payload.data)
+}
+function api_v1_scar_retrieve(payload) {
+  return testscarmodeldevAPI.get(`/api/v1/scar/${payload.id}/`)
+}
+function api_v1_scar_update(payload) {
+  return testscarmodeldevAPI.put(`/api/v1/scar/${payload.id}/`, payload.data)
+}
+function api_v1_scar_partial_update(payload) {
+  return testscarmodeldevAPI.patch(`/api/v1/scar/${payload.id}/`, payload.data)
+}
+function api_v1_scar_destroy(payload) {
+  return testscarmodeldevAPI.delete(`/api/v1/scar/${payload.id}/`)
 }
 function rest_auth_logout_retrieve(payload) {
   return testscarmodeldevAPI.get(`/rest-auth/logout/`)
@@ -54,6 +72,8 @@ function rest_auth_registration_verify_email_create(payload) {
   )
 }
 export const apiService = {
+  api_v1_scar_list,
+  api_v1_scar_create,
   api_v1_login_create,
   api_v1_signup_create,
   rest_auth_user_retrieve,
@@ -61,6 +81,10 @@ export const apiService = {
   rest_auth_user_partial_update,
   api_docs_schema_retrieve,
   rest_auth_login_create,
+  api_v1_scar_retrieve,
+  api_v1_scar_update,
+  api_v1_scar_partial_update,
+  api_v1_scar_destroy,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
   rest_auth_registration_create,
